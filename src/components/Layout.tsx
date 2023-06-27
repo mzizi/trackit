9e742bfd0622d6f350f2d3ef3173e6482f6ee150
Rodgers Gitau <rodgersgitau@gmail.com>
2023-06-27 14:32:42 +0300

    feat: custom landing page
    - add `Footer` component to `Layout`

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { FC, ReactNode } from "react";
 import { ToastContainer } from "react-toastify";
 
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 import type { NavLink } from "@/types";
@@ -13,16 +14,19 @@ interface Props {
 
 const Layout: FC<Props> = ({ children, links }) => {
   return (
-    <main className="flex flex-col min-h-screen gap-4">
-      <nav className="sticky inset-0 w-full h-full py-4 border-b-2  bg-accent lg:px-24 border-b-border">
-        <Navbar links={links} />
-      </nav>
+    <>
+      <main className="relative flex flex-col h-full min-h-screen gap-4">
+        <nav className="sticky inset-0 w-full h-full py-4 border-b-2 bg-accent lg:px-24 border-b-border">
+          <Navbar links={links} />
+        </nav>
 
-      <section className="flex flex-col justify-center flex-1 w-full px-2 lg:px-24">
-        {children}
-      </section>
+        <section className="flex flex-col justify-center flex-1 w-full px-2 lg:px-24">
+          {children}
+        </section>
+        <Footer />
+      </main>
       <ToastContainer />
-    </main>
+    </>
   );
 };
 
--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-"use client";
-
-export { default as Layout } from "./Layout";
