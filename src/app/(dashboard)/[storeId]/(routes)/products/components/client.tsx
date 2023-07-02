9ed9efb6177064cfb8f159011f1671a8896fdd8e
Rodgers Gitau <rodgersgitau@gmail.com>
2023-07-02 18:58:05 +0300

    feat: products subroute

--- src/app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -5,7 +5,7 @@ import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
-import { Heading } from "@/components/ui/header";
+import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { ApiList } from "@/components/ui/api-list";
 
