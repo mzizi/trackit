e04bed81fa69fdec2beeef6ab2d926ced19cd9da
Rodgers Gitau <rodgersgitau@gmail.com>
2023-07-02 18:58:23 +0300

    feat: settings subroute

---
+++ src/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,35 @@
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+
+import prisma from "@/utils/prisma";
+
+import { SettingsForm } from "./components/settings-form";
+
+const SettingsPage = async ({ params }: { params: { storeId: string } }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prisma.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
